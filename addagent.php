9544b95b25bcc434db01cda8892dbedba3ddc27a
alessio.scicolone <scicolone.alessio@gmail.com>
2016-03-21 20:54:27 +0100

    New Graphics

--- addagent.php
+++ addagent.php
@@ -34,18 +34,44 @@ if($action == 'add' || $action == 'mod'){
 		echo('<form method="POST" action="index.php?section=addagent&action=insert">');
 	else
 		echo('<form method="POST" action="index.php?section=addagent&action=update&id='.$id.'">');
-	
-	echo('Nome: <input type="text" name="nome" value="'.$nome.'" required="required">');
-	echo('Cognome: <input type="text" name="cognome" value="'.$cognome.'" required="required"><br>');
-	echo('Codice fiscale: <input type="text" name="codicefiscale" value="'.$codfisc.'" pattern="^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$" required="required">');
-	echo('Partita IVA: <input type="text" value="'.$partitaiva.'" name="partitaiva">');
-	echo('E-Mail: <input type="email" name="email" value="'.$email.'" required="required"><br>');
-	echo('% IVA: <input type="number" value="'.$iva.'" name="iva">');
-	echo('% Enasarco <input type="number" value="'.$enasarco.'" name="enasarco"><br>');
-	echo('% Ritenuta d\'acconto <input type="number" value="'.$ritacconto.'" name="ritacconto"><br>');
-	echo('Tipo contributo INPS <select name="tipoinps" selected="'.$tipoinps.'"><option value="0">-</option><option value="1">Tipo1</option><option value="2">Tipo2</option></select>');
-	echo('% Contributo INPS <input type="number" name="contributoinps" value="'.$contributoinps.'">');
+	echo('<div align="center" class="CSS_Table_Example" style="width:600px;height:150px;">
+			<table >
+				<tr> ');
+	echo('<td>');
+	echo('Nome: </td><td><input type="text" name="nome" value="'.$nome.'" required="required">');
+	echo('</td></tr>');
+	echo('<tr> <td>');
+	echo('Cognome: </td><td><input type="text" name="cognome" value="'.$cognome.'" required="required"><br>');
+	echo('</td></tr>');
+	echo('<tr> <td>');
+	echo('Codice fiscale: </td><td><input type="text" name="codicefiscale" value="'.$codfisc.'" pattern="^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$" required="required">');
+	echo('</td></tr>');	
+	echo('<tr> <td>');
+	echo('Partita IVA: </td><td><input type="text" value="'.$partitaiva.'" name="partitaiva">');
+	echo('</td></tr>');
+	echo('<tr> <td>');
+	echo('E-Mail: </td><td><input type="email" name="email" value="'.$email.'" required="required"><br>');
+	echo('</td></tr>');
+	echo('<tr> <td>');
+	echo('% IVA: </td><td><input type="number" value="'.$iva.'" name="iva">');
+	echo('</td></tr>');
+	echo('<tr> <td>');
+	echo('% Enasarco </td><td><input type="number" value="'.$enasarco.'" name="enasarco"><br>');
+	echo('</td></tr>');
+	echo('<tr> <td>');
+	echo('% Ritenuta d\'acconto </td><td><input type="number" value="'.$ritacconto.'" name="ritacconto"><br>');
+	echo('</td></tr>');
+	echo('<tr> <td>');
+	echo('Tipo contributo INPS </td><td><select name="tipoinps" selected="'.$tipoinps.'"><option value="0">-</option><option value="1">Tipo1</option><option value="2">Tipo2</option></select>');
+	echo('</td></tr>');
+	echo('<tr> <td>');
+	echo('% Contributo INPS </td><td><input type="number" name="contributoinps" value="'.$contributoinps.'">');
+	echo('</td></tr>');
+	echo('<tr> <td>');
 	echo('<input type="submit" name="Invia">');
+	echo('</td></tr>');
+        echo('</table>
+		</div>');
 	echo('</form>');
 }
 
--- agentlist.php
+++ agentlist.php
@@ -3,10 +3,69 @@ include('db.php');
 $query = $db->prepare('SELECT * FROM agenti');
 $query->execute();
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo('<html>
+
+	<head>
+		<link rel="stylesheet" href="tableagentlist.css" type="text/css"/>	
+	</head>
+
+	<body>');
+
 echo('<a href="index.php?section=addagent&action=add">Aggiungi nuovo agente</a>');
-echo('<table border="1"><tr><th>Nome</th><th>Cognome</th><th>Codice Fiscale</th><th>P.IVA</th><th>e-mail</th><th></th><th></th></tr>');
+echo('<div  class="CSS_Table_Example" style="width:600px;height:150px;" > ');
+echo('              <table >
+                    <tr>
+                        <td>
+                            Nome
+                        </td>
+                        <td >
+                            Cognome
+                        </td>
+                        <td>
+                            Codice Fiscale
+                        </td>
+			<td>
+                            P.IVA
+                        </td>
+			<td>
+                            e-mail
+                        </td>
+			<td>
+                            dettagli
+                        </td>
+                    </tr>');
 foreach ($results as $row){
-	echo('<tr><td>'.$row['nome'].'</td><td>'.$row['cognome'].'</td><td>'.$row['codicefiscale'].'</td><td>'.$row['partitaiva'].'</td><td>'.$row['email'].'</td><td><a href="index.php?section=viewagent&id='.$row['id'].'">dettagli</a></td></tr>');
+ echo('            <tr>
+                        <td >
+                            '.$row['nome'].'
+                        </td>
+                        <td>
+                            '.$row['cognome'].'
+                        </td>
+                        <td>
+                            '.$row['codicefiscale'].'
+                        </td>
+			<td>
+                            '.$row['partitaiva'].'
+                        </td>
+			<td>
+                            '.$row['email'].'
+                        </td>
+			<td>
+                            <a href="index.php?section=viewagent&id='.$row['id'].'">dettagli</a>
+                        </td>
+                    </tr>');
+                   
 }
-echo('</table>');	
+echo('              </table>
+            </div>
+	</body>	
+</html>');
+
+//echo('<table border="1"><tr><th>Nome</th><th>Cognome</th><th>Codice Fiscale</th><th>P.IVA</th><th>e-mail</th><th></th><th></th></tr>');
+//foreach ($results as $row){
+//	echo('<tr><td>'.$row['nome'].'</td><td>'.$row['cognome'].'</td><td>'.$row['codicefiscale'].'</td><td>'.$row['partitaiva'].'</td><td>'.$row['email'].'</td><td><a href="index.php?section=viewagent&id='.$row['id'].'">dettagli</a></td></tr>');
+//}
+//echo('</table>');	
 ?>
