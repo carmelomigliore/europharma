6ccfd19a6c39f6b656bfaa4e3c1094cca501c754
Carmelo Migliore <car.miglio@gmail.com>
2016-04-24 20:13:33 +0200

    Statistics completed

--- globalstats.php
+++ globalstats.php
@@ -90,10 +90,14 @@ if($action=='results'){
 	$idprodotto = $_POST['idprodotto'];
 	$annomesestart = $_POST['annomesestart'];
 	$annomeseend = $_POST['annomeseend'];
+	$annomeseconfrontostart = $_POST['annomeseconfrontostart'];
+	$annomeseconfrontoend = $_POST['annomeseconfrontoend'];
 	$microarea = $_POST['microarea'];
 	$provincia = $_POST['provincia'];
 	$regione = $_POST['regione'];
 	$progressivo = $_POST['progressivo']?true:false;
+	$raggruppa = $_POST['raggruppa'];
+	$raggruppacollaboratori = $_POST['raggruppacollaboratori']?true:false;
 
 
 	/*$tipoquery = 'ims';
@@ -106,17 +110,17 @@ if($action=='results'){
 	$progressivo = false;*/
 
 	if($tipoquery == 'ims'){
-		$select.= 'sum(numeropezzi) AS numeropezzi, sum(numeropezzi* prezzonetto) AS nettofatturato, sum(numeropezzi* prezzonetto * provvigione/100) AS provvigioni, prodotti.nome';
+		$select.= 'prodotti.nome AS "Prodotto", sum(numeropezzi) AS "Numero Pezzi", replace(to_char(sum(numeropezzi* prezzonetto), \'FM999999999.00\'),\'.\',\',\') AS "Netto Fatturato", replace(to_char(sum(numeropezzi* prezzonetto * provvigione/100), \'FM999999999.00\'),\'.\',\',\') AS "Provvigioni"';
 		$from = 'storico, prodotti';
 		$where.= 'idprodotto = prodotti.id AND ';
 	} 
 	else if($tipoquery == 'farmacie'){
-		$select.= 'sum(numeropezzi) AS numeropezzi, sum(numeropezzi* prezzonetto) AS nettofatturato, sum(numeropezzi* prezzonetto * provvigione/100) AS provvigioni, prodotti.nome';
+		$select.= 'prodotti.nome AS "Prodotto", sum(numeropezzi) AS "Numero Pezzi", replace(to_char(sum(numeropezzi* prezzonetto), \'FM999999999.00\'),\'.\',\',\') AS "Netto Fatturato", replace(to_char(sum(numeropezzi* prezzonetto * provvigione/100), \'FM999999999.00\'),\'.\',\',\') AS "Provvigioni"';
 		$from = '"compensi-farmacie", prodotti';
 		$where.= 'idprodotto = prodotti.id AND ';
 	}
 	else if($tipoquery == 'farmacieims'){
-		$select.= 'sum(numeropezzi) AS numeropezzi, sum(nettofatturato) AS nettofatturato, sum(provvigioni) AS provvigioni, nome';
+		$select.= 'total.nome AS "Prodotto", sum(numeropezzi) AS "Numero Pezzi", replace(to_char(sum(nettofatturato), \'FM999999999.00\'),\'.\',\',\') AS "Netto Fatturato", replace(to_char(sum(provvigioni), \'FM999999999.00\'),\'.\',\',\') AS "Provvigioni"';
 		$from = '(SELECT numeropezzi, (numeropezzi* prezzonetto) AS nettofatturato, (numeropezzi* prezzonetto * provvigione/100) AS provvigioni, prodotti.nome, annomese, idagente, idprodotto 
 			FROM storico, prodotti WHERE idprodotto = prodotti.id 
 			UNION ALL
@@ -133,16 +137,7 @@ if($action=='results'){
 		$where.="idprodotto = $idprodotto AND ";
 	}
 
-	if($annomesestart!=-1 && $annomeseend==-1){
-		$where.="annomese = $annomesestart::varchar AND ";
-	}
-	else if($annomesestart!=-1 && $annomeseend!=-1){
-		$where.='annomese = ANY (\'{'.php_to_postgres_array(getMesiIntervallo($annomesestart, $annomeseend)).'}\'::varchar[]) AND ';
-		if(!$progressivo){
-			$select.=', annomese';
-			$groupby.="annomese, ";
-		}
-	}
+	
 
 	if($tipoquery == 'ims'){
 		if(!is_null($microarea) && $microarea!=-1){
@@ -156,22 +151,152 @@ if($action=='results'){
 		}
 		
 		if($raggruppa=='area'){
-			$select.= ", codarea";
-			$groupby.="codarea, ";
+			$from.=', aree';
+			$where.= "aree.codice = codarea AND ";
+			$select.= ", (aree.nome || ' ' || substring(codarea,4,2)) AS \"Microarea\"";
+			$groupby.="codarea, aree.nome, ";
 		}else if($raggruppa=='provincia'){
 			$from.=', aree';
 			$select.= ", aree.nome AS provincia";
 			$where.= "aree.codice = codarea AND ";
 			$groupby.="provincia, ";
-		}
+		} 
+	}
+	
+	if($raggruppacollaboratori){
+		$from.=', agenti';
+		$where.='agenti.id = idagente AND ';
+		$select.=", (agenti.nome || ' ' || agenti.cognome) AS \"Collaboratore\"";
+		$groupby.= "agenti.nome, agenti.cognome, ";
 	}
 	
 
-	$groupby.="idprodotto, nome";
-	$orderby.="nome";
+	
+	
+	$where2 = $where;
+	
+	if($annomesestart!=-1 && $annomeseend==-1){
+		
+		$where.="annomese = $annomesestart::varchar AND ";
+	}
+	else if($annomesestart!=-1 && $annomeseend!=-1){
+		$where.='annomese = ANY (\'{'.php_to_postgres_array(getMesiIntervallo($annomesestart, $annomeseend)).'}\'::varchar[]) AND ';
+		if(!$progressivo){
+			$select.=', annomese';
+			$groupby.="annomese, ";
+		}
+	}
+	
+	if($tipoquery!='farmacieims'){
+		$groupby.="idprodotto, prodotti.nome";
+		$orderby.="prodotti.nome";
+	}
+	else{
+		$groupby.="idprodotto, total.nome";
+		$orderby.="total.nome";
+	}
 
 	$sql = "SELECT $select FROM $from WHERE $where TRUE GROUP BY $groupby ORDER BY $orderby";
-	echo $sql;
+	
+	
+	if($annomeseconfrontostart!=-1){
+		if($annomeseconfrontostart!=-1 && $annomeseconfrontoend==-1){
+			$where2.="annomese = $annomeseconfrontostart::varchar AND ";
+		}
+		else if($annomeseconfrontostart!=-1 && $annomeseconfrontoend != -1){
+			$where2.='annomese = ANY (\'{'.php_to_postgres_array(getMesiIntervallo($annomeseconfrontostart, $annomeseconfrontoend)).'}\'::varchar[]) AND ';
+		}
+		$sql2 = "SELECT $select FROM $from WHERE $where2 TRUE GROUP BY $groupby ORDER BY $orderby";
+		
+		/*$query = $db->prepare($sql2);
+		$query->execute();
+		$results = $query->fetchAll(PDO::FETCH_NUM);
+		$columns = array();
+		$rs = $db->query('WITH myquery AS ('.$sql2.') SELECT * FROM myquery LIMIT 0');
+		for ($i = 0; $i < $rs->columnCount(); $i++) {
+		    $col = $rs->getColumnMeta($i);
+		    $columns[] = $col['name'];
+		}
+	
+		echo('<br><br><table border="1">');
+		echo('<tr>');
+		foreach ($columns as $column){
+			echo('<td>'.$column.'</td>');
+		}
+		echo('</tr>');
+	
+		foreach($results as $row){
+			echo('<tr>');
+			foreach($row as $value){
+				echo('<td>'.$value.'</td>');
+			}
+			echo('</tr>');
+		}
+		echo('</table>');*/
+		$where3 = 'a."Prodotto" = b."Prodotto" AND ';
+		$moreselect='';
+		if(!$progressivo && $annomesestart!=-1 && $annomeseend!=-1){
+			$where3.='a.annomese = b.annomese AND ';
+			$moreselect.='a.annomese, ';
+		}
+		if($raggruppacollaboratori){
+			$where3.='a."Collaboratore" = b."Collaboratore" AND ';
+			$moreselect.='a."Collaboratore", ';
+		}
+		
+		if($tipoquery == 'ims'){
+			if($raggruppa=='area'){
+				$where3.='a."Microarea" = b."Microarea" AND ';
+				$moreselect.='a."Microarea", ';
+			}else if($raggruppa=='provincia'){
+				$where3.='a.provincia = b.provincia AND ';
+				$moreselect.='a.provincia, ';
+			} 
+		}
+	
+		
+		
+		$sqltot = 'SELECT a."Prodotto", '.$moreselect.' COALESCE(a."Numero Pezzi",0) AS "Numero Pezzi 1", COALESCE(b."Numero Pezzi",0) AS "Numero Pezzi 2", replace(to_char(((COALESCE(a."Numero Pezzi",0) - COALESCE(b."Numero Pezzi",0))/NULLIF(COALESCE(b."Numero Pezzi",0),0)*100), \'FM999999999.00\'),\'.\',\',\') AS "Diff pezzi %", COALESCE(a."Netto Fatturato",\'0\') AS "Netto Fatturato 1", COALESCE(b."Netto Fatturato",\'0\') AS "Netto Fatturato 2", replace(to_char((((replace(COALESCE(a."Netto Fatturato",\'0\'), \',\',\'.\')::real - replace(COALESCE(b."Netto Fatturato",\'0\'), \',\',\'.\')::real)/NULLIF(replace(COALESCE(b."Netto Fatturato",\'0\'), \',\',\'.\')::real,0))*100), \'FM999999999.00\'),\'.\',\',\') AS "Diff netto fatturato %", COALESCE(a."Provvigioni",\'0\') AS "Provvigioni 1", COALESCE(b."Provvigioni",\'0\') AS "Provvigioni 2", replace(to_char((((replace(COALESCE(a."Provvigioni",\'0\'), \',\',\'.\')::real - replace(COALESCE(b."Provvigioni",\'0\'), \',\',\'.\')::real)/NULLIF(replace(COALESCE(b."Provvigioni",\'0\'), \',\',\'.\')::real,0))*100), \'FM999999999.00\'),\'.\',\',\') AS "Diff provvigioni %"
+		FROM ('.$sql.') AS a FULL JOIN ('.$sql2.') AS b ON '.$where3.' TRUE';
+		
+		//echo($sqltot);
+		$sql = $sqltot;	
+	}
+	$query = $db->prepare($sql);
+	$query->execute();
+	
+	$results = $query->fetchAll(PDO::FETCH_NUM);
+	$columns = array();
+	$rs = $db->query('WITH myquery AS ('.$sql.') SELECT * FROM myquery LIMIT 0');
+	for ($i = 0; $i < $rs->columnCount(); $i++) {
+	    $col = $rs->getColumnMeta($i);
+	    $columns[] = $col['name'];
+	}
+	
+	$fp = fopen('temp/statistics.csv', 'w');	
+	fputcsv($fp, $columns, ';');
+	foreach ($results as $fields) {
+	    fputcsv($fp, $fields, ';');
+	}
+	fclose($fp);
+
+	echo('<table border="1">');
+	echo('<tr>');
+	foreach ($columns as $column){
+		echo('<td>'.$column.'</td>');
+	}
+	echo('</tr>');
+
+	foreach($results as $row){
+		echo('<tr>');
+		foreach($row as $value){
+			echo('<td>'.$value.'</td>');
+		}
+		echo('</tr>');
+	}
+	echo('</table>');
+	echo('<a href="temp/statistics.csv" download>Scarica questa tabella in CSV</a>');
+	
 }
 
 if($action=='form'){
@@ -212,6 +337,18 @@ if($action=='form'){
 		echo('<option value="'.$annomese['annomese'].'">'.$annomese['annomese'].'</option>');
 	}
 	echo('</select></td>');
+	echo('<tr><td>Anno e mese confronto(start): </td><td><select name="annomeseconfrontostart">');
+	echo('<option value="-1">-</option>');
+	foreach($annimese as $annomese){
+		echo('<option value="'.$annomese['annomese'].'">'.$annomese['annomese'].'</option>');
+	}
+	echo('</select></td>');
+	echo('<td>Anno e mese confronto (end): </td><td><select name="annomeseconfrontoend">');
+	echo('<option value="-1">-</option>');
+	foreach($annimese as $annomese){
+		echo('<option value="'.$annomese['annomese'].'">'.$annomese['annomese'].'</option>');
+	}
+	echo('</select></td>');
 	echo('<td>Progressivo: <input type="checkbox" name="progressivo" value="true"></td></tr>');
 	echo('<tr><td>Regione: </td><td><select name="regione" onchange="fetch_provincie(this.value);">');
 	echo('<option value="-1">-</option>');
@@ -221,9 +358,11 @@ if($action=='form'){
 	echo('</select></td>');
 	echo('<td>Provincia: </td><td><select name="provincia" id="provincia" onchange="fetch_aree(this.value);"></select></td>');
 	echo('<td>Microarea: </td><td><select name="microarea" id="microarea"></select></td></tr>');
-	echo('<tr><td><input type="radio" name="raggruppa" value="none" checked>Non raggruppare</td>
+	echo('<tr><td>Attenzione: i filtri geografici hanno effetto solo sulle query di tipo IMS</td></tr>');
+	echo('<tr><td><input type="radio" name="raggruppa" value="none" checked>Non raggruppare per aree</td>
 	<td><input type="radio" name="raggruppa" value="provincia">Raggruppa per provincia</td>
-  	<td><input type="radio" name="raggruppa" value="area">Raggruppa per microarea</td></tr>');
+  	<td><input type="radio" name="raggruppa" value="area">Raggruppa per microarea</td>
+  	<td><input type="checkbox" name="raggruppacollaboratori" value="true">Raggruppa per collaboratori</td></tr>');
 	echo('<tr><td><input type="submit" name="invia"></td></tr>');
 	echo('</table></form>');
 }
